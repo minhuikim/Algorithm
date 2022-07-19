067e952a05a8a0df45960c84734a41f7ade8dfe9
minhuikim <kmh940@gmail.com>
2022-07-19 22:45:50 +0900

    python 기본 연습

--- python/practice8_7.py
+++ python/practice8_7.py
@@ -20,5 +20,18 @@ class House:
 
     # 매물 정보 표시
     def show_detail(self):
-        print("{0} {1} {2} {3} {4}".format(self.location, self.house_type, self.deal_type, self.price, self.completion_year))
+        print(self.location, self.house_type, self.deal_type, self.price, self.completion_year)
+        
+houses = []
+house1 = House("강남", "아파트", "매매", "10억", "2010년")
+house2 = House("마포", "오피스텔", "전세", "5억", "2007년")
+house3 = House("송파", "빌라", "월세", "500/50", "2000년")
 
+houses.append(house1)
+houses.append(house2)
+houses.append(house3)
+
+print("총 {0}대의 매물이 있습니다.".format(len(houses)))
+
+for h in houses:
+    House.show_detail(h)
