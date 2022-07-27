0781bc65ac7d763704183387b55ce0aef6b6e0c0
minhuikim <kmh940@gmail.com>
2022-07-27 23:59:29 +0900

    2231 분해합

--- python/2231_dec_sum.py
+++ python/2231_dec_sum.py
@@ -1,23 +1,21 @@
 num = int(input())
 
-if num<10:
-    print(0)
+if num<=10:
+    print(num)
 else:
     for n in range(10, num):
         i = int(10)
         sum = int(n)
-        m = int(n)
+        m = int(0)
 
-        while i < n:
+        while i <= n:
             if sum == n:
                 sum += n % i
             sum += n // i % i
             i *= 10
         
         if sum == num:
+            m = int(n)
             break
-        elif n == num:
-            m = 0
-            break 
 
-    print(n)
+    print(m)
