0bcf496cd04216a87ff6fc26fa1ede22cebf044d
minhuikim <kmh940@gmail.com>
2022-08-02 23:45:39 +0900

    python 기본 연습

--- python/practice/practice9_1.py
+++ python/practice/practice9_1.py
@@ -15,29 +15,25 @@ chicken = 10
 waiting = 1             # 홀 안에는 현재 만석. 대기번호 1부터 시작
 
 class SoldOutError(Exception):
-    def __init__(self, msg):
-        self.msg = msg
-
-    def __str__(self):
-        return self.msg
-
+    pass
 
 while(True):
     try:
         print("[남은 치킨 : {0}]".format(chicken))
         order = int(input("치킨 몇 마리 주문하시겠습니까?"))
-        if order < 1:
-            raise ValueError
+       
         if order > chicken: # 남은 치킨보다 주문량이 많을때
             print("재료가 부족합니다.")
+        elif order <= 0:
+            raise ValueError
         else:
             print("[대기번호 {0}] {1} 마리 주문이 완료되었습니다.".format(waiting, order))
             waiting += 1
             chicken -= order
         if chicken < 1:
-            raise SoldOutError("재고가 소진되어 더 이상 주문을 받지 않습니다.")
+            raise SoldOutError()
     except ValueError:
         print("잘못된 값을 입력하였습니다.")
-    except SoldOutError as err:
-        print(err)
+    except SoldOutError:
+        print("재고가 소진되어 더 이상 주문을 받지 않습니다.")
         break
