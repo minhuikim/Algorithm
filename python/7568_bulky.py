bd24af6e5172e8f39fec7845a62354928a43d3d1
minhuikim <kmh940@gmail.com>
2022-08-04 23:47:39 +0900

    7568 덩치

---
+++ python/7568_bulky.py
@@ -0,0 +1,18 @@
+mans = int(input())
+# body = []
+x = y = []
+
+for n in range(0,mans):
+    x[n] = input()
+    y[n] = input()
+
+#     body[n].append[x, y, 1]
+
+#     if n > 0:
+#         for m in range(0, n):
+#             if int(body[m].x) < int(x) & int(body[m].y) < int(y):
+#                 body[n][2].append(body[n][2] + 1)
+
+
+# for n in range(0,mans):
+#     print(body[n][2])
