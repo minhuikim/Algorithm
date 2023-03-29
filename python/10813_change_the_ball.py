b05968ebe6c733f02d03360378fb17e86659cd90
minhuikim <kmh940@gmail.com>
2023-03-29 22:26:28 +0900

    10813 공 바꾸기 수정

--- python/10813_change_the_ball.py
+++ python/10813_change_the_ball.py
@@ -1,17 +1,12 @@
 n, m = map(int, input().split())
-basket = []
-
-for k in range(n):
-    basket.append(k + 1)
+basket = list(range(1, n+1))
 
 for k in range(m):
     i, j = map(int, input().split())
-    i = i - 1
-    j = j - 1
 
-    tmp = basket[i]
-    basket[i] = basket[j]
-    basket[j] = tmp
+    tmp = basket[i-1]
+    basket[i-1] = basket[j-1]
+    basket[j-1] = tmp
 
 for k in range(n):
     print(basket[k], end=" ")
