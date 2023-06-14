8a542c660d517db1a89eba0c0f94edb971516a4f
minhuikim <kmh940@gmail.com>
2023-06-14 19:39:36 +0900

    27323 직사각형

--- python/unit_test.py
+++ python/unit_test.py
@@ -1,1 +1,24 @@
-print(1/0)
+# print(25 % 7)
+# print(5 / 2)
+# print(5 // 2)
+# print(round(3.141592))
+# print(round(3.4))
+# print(abs(-3))
+# print(pow(2, 3))
+# print("""출력
+#     테스트""")
+print(format(3.141592, ">20.3f"))
+
+               3.142
+
+# x = 1
+
+# def inc():
+#     y = x + 1
+#     print("inc", y)
+
+# inc()
+
+# print(x)
+# print(y)
+
