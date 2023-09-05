41f9bda255329ab5c24e6924ed8a58060741acd5
minhuikim <kmh940@gmail.com>
2023-09-05 23:48:40 +0900

    20230905 약수의 개수와 덧셈_다른사람풀이

--- python/programmers/level1/number_of_factors_and_addition.py
+++ python/programmers/level1/number_of_factors_and_addition.py
@@ -1,17 +1,27 @@
+# def solution(left, right):
+#     answer = 0
+#     for i in range(left, right+1):
+#         div_list = []
+#         for j in range(1, int(i**(1/2))+1):
+#             if (i % j) == 0:
+#                 div_list.append(j // 1)
+#                 if (j**2) != i:
+#                     div_list.append(i // j)
+#         if len(div_list) % 2 == 0:
+#             answer += i
+#         else:
+#             answer -= i
+#         print(div_list, answer)
+#     return answer
+
 def solution(left, right):
     answer = 0
-    for i in range(left, right+1):
-        div_list = []
-        for j in range(1, int(i**(1/2))+1):
-            if (i % j) == 0:
-                div_list.append(j // 1)
-                if (j**2) != i:
-                    div_list.append(i // j)
-        if len(div_list) % 2 == 0:
-            answer += i
-        else:
+    for i in range(left,right+1):
+        print(i, int(i**0.5), i**0.5)
+        if int(i**0.5)==i**0.5:
             answer -= i
-        print(div_list, answer)
+        else:
+            answer += i
     return answer
 
 left = 24
