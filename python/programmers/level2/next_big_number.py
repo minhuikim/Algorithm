660ccf7eb1b1de91940cb49b2b4a67c9d33168f8
minhuikim <kmh940@gmail.com>
2023-10-12 23:04:27 +0900

    20231012 다음 큰 숫자

--- python/programmers/level2/next_big_number.py
+++ python/programmers/level2/next_big_number.py
@@ -1,10 +1,10 @@
 def solution(n):
     answer = n
-    bin_n_cnt = bin(n).count("1");
+    bin_n_cnt = bin(n).count("1")
     while True:
         answer += 1
         if bin_n_cnt == bin(answer).count("1"):
-            break;
+            break
     return answer
 
 n = 78
